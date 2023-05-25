ed3d929b8c78b670c33ee05b86bb1bd4330de1ea
Zeynep Buse Sevincel <zeynepbusesevincel@Zeyneps-MacBook-Pro.local>
2023-05-25 16:56:33 -0400

    working on Lab02

--- lab02/ecs/test/DateNextDateExceptionTest.java
+++ lab02/ecs/test/DateNextDateExceptionTest.java
@@ -1,5 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class DateNextDateExceptionTest
 {
-
-
+  public void assertNextDateException(Date date) {
+    assertThrows(IllegalArgumentException.class, () -> {
+      date.nextDate();
+    });
+  }
 }
