e29648070bdb3e24585b7fb5289a942966008857
Zeynep Buse Sevincel <zeynepbusesevincel@Zeyneps-MacBook-Pro.local>
2023-06-07 22:45:28 -0400

    working on lab 3

--- lab03/date/test/DateTest.java
+++ lab03/date/test/DateTest.java
@@ -149,4 +149,118 @@ class DateTest {
     );
   }
 
+  @Test
+  void day_invalid_tc21() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(2021, 5, 32)
+    );
+  }
+
+  @Test
+  void day_invalid_tc22() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(2021, 4, 31)
+    );
+  }
+
+  @Test
+  void day_invalid_tc23() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(2000, 2, 30)
+    );
+  }
+
+  @Test
+  void toString_tc24() {
+    Date d = new Date(2021, 4, 13);
+    assertEquals(d.toString(), "2021/April/13");
+  }
+
+  @Test
+  void endOfMonth_tc25() {
+    Date d = new Date(2021, 2, 28);
+    assertEquals(d.nextDate(), new Date(2021, 3, 1));
+  }
+
+  @Test
+  void endOfMonth_tc26() {
+    Date d = new Date(2021, 4, 30);
+    assertEquals(d.nextDate(), new Date(2021, 5, 1));
+  }
+
+  @Test
+  void endOfMonth_tc27() {
+    Date d = new Date(2021, 6, 30);
+    assertEquals(d.nextDate(), new Date(2021, 7, 1));
+  }
+
+  @Test
+  void endOfMonth_tc28() {
+    Date d = new Date(2021, 9, 30);
+    assertEquals(d.nextDate(), new Date(2021, 10, 1));
+  }
+
+  @Test
+  void endOfMonth_tc29() {
+    Date d = new Date(2021, 11, 30);
+    assertEquals(d.nextDate(), new Date(2021, 12, 1));
+  }
+
+  @Test
+  void endOfMonth_tc30() {
+    Date d = new Date(2020, 2, 29);
+    assertEquals(d.nextDate(), new Date(2020, 3, 1));
+  }
+
+  @Test
+  void endOfMonth_tc31() {
+    Date d = new Date(2020, 2, 28);
+    assertEquals(d.nextDate(), new Date(2020, 2, 29));
+  }
+
+  @Test
+  void equals_tc32() {
+    Date d = new Date(2021, 5, 4);
+    assertEquals(false, d.equals(1));
+  }
+
+  @Test
+  void equals_tc33() {
+    Date d1 = new Date(2021, 5, 4);
+    Date d2 = new Date(2021, 5, 5);
+    assertEquals(false, d1.equals(d2));
+  }
+
+  @Test
+  void equals_tc34() {
+    Date d1 = new Date(2021, 5, 4);
+    Date d2 = new Date(2021, 6, 4);
+    assertEquals(false, d1.equals(d2));
+  }
+
+  @Test
+  void equals_tc35() {
+    Date d1 = new Date(2021, 5, 4);
+    Date d2 = new Date(2022, 5, 4);
+    assertEquals(false, d1.equals(d2));
+  }
+
+  @Test
+  void month_invalid_tc36() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1975, 13, 10)
+    );
+  }
+
+  @Test
+  void month_invalid_tc37() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1975, -4, 10)
+    );
+  }
 }
